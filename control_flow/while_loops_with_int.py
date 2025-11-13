5b0d46caa0c6282285a33b3308912715b8761534
sameershorif <sameershorif@gmail.com>
2025-11-13 22:29:18 +0000

    while loops with int

---
+++ control_flow/while_loops_with_int.py
@@ -0,0 +1,16 @@
+# x needs to be initialised, otherwise the loop doesn't know what x is
+x = 0
+while x < 10:
+    print(f"x -> {x}")
+    # If we don't increment x, the while condition will ALWAYS be true → infinite loop
+    x = x + 1
+
+
+x = 0
+while x < 10:
+    print(f"x -> {x}")
+
+    if x == 4:
+        break  # stop the loop when x reaches 4
+
+    x = x + 1
